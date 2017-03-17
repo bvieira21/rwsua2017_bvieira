d4810d0c03b067078d44f41623583fa263b0422a
bvieira21 <bvieira21@hotmail.com>
2017-03-17 14:39:49 +0000

    seek and hunt implemented

--- player_bvieira/src/player_bvieira_node.cpp
+++ player_bvieira/src/player_bvieira_node.cpp
@@ -24,7 +24,9 @@
 #include <tf/transform_listener.h>
 
 #include <rwsua2017_libs/player.h>   
-#include <rwsua2017_msgs/MakeAPlay.h>                                                                            
+#include <rwsua2017_msgs/MakeAPlay.h>
+
+#include <visualization_msgs/Marker.h>
 
 using namespace std;
 
@@ -35,10 +37,9 @@ namespace rwsua2017 {
     public:
 
         ros::Subscriber sub;
-
         tf::TransformListener listener;
-
         tf::TransformBroadcaster br;
+        ros::Publisher vis_pub;
 
         /**
          * @brief  Class constructor, initializes the player 
@@ -50,7 +51,8 @@ namespace rwsua2017 {
         MyPlayer(string argin_name, string argin_team_name) : Player(argin_name, argin_team_name) {
 
             sub = n.subscribe("/make_a_play/cat", 1000, &MyPlayer::makeAPlayCallback, this);
-            cout << "Initialized MyPlayer" << endl;
+
+            vis_pub = n.advertise<visualization_msgs::Marker>("/bocas", 0);
 
             tf::Transform t1;
             t1.setOrigin(tf::Vector3(1, 1, 0));
@@ -136,13 +138,47 @@ namespace rwsua2017 {
             //definição dos angulos de rotação e valores de translação
             //deveria ser calculado pela AI do sistema
 
+            //string neaby_players = getNearbyPlayers(); //in 1 is the prey, 2 the hunter
+            std::vector<std::string> enemies = getNearbyPlayers();
+            float turn_angle;
             
+            if (enemies[2]=="run")
+                turn_angle=getAngleTo(enemies[1])+5*M_PI/6;
+            else
+                turn_angle=getAngleTo(enemies[0]);
 
-            float turn_angle = getAngleTo(getNearbyPrey());
-
+            
             float displacement = msg->max_displacement;
 
+
+
+            //move my player
             move(displacement, turn_angle, msg->max_displacement, M_PI / 30);
+
+            //enviar boca
+            visualization_msgs::Marker marker;
+            marker.header.frame_id = name;
+            marker.header.stamp = ros::Time();
+            marker.ns = name;
+            marker.id = 0;
+            marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = 0;
+            marker.pose.position.y = 0.4;
+            marker.pose.position.z = 0;
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+            marker.scale.z = 0.4;
+            marker.color.a = 1.0; // Don't forget to set the alpha!
+            marker.color.r = 0.3;
+            marker.color.g = 0.3;
+            marker.color.b = 0.3;
+            marker.frame_locked = 1;
+            marker.lifetime = ros::Duration(1);
+            marker.text = "atencao: isto e uma boca!";
+            vis_pub.publish(marker);
         }
 
         /**
@@ -173,9 +209,9 @@ namespace rwsua2017 {
             br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "/map", name));
         }
 
-        string getNearbyPrey() {
+        std::vector<std::string> getNearbyPlayers() {
 
-            tf::StampedTransform trans1, trans2, trans3;
+            tf::StampedTransform trans1, trans2, trans3, trans4, trans5, trans6;
             ros::Time now = ros::Time(0); //gets the latest transform received
             try {
                 listener.waitForTransform(name, "jferreira", now, ros::Duration(time_to_wait));
@@ -187,17 +223,29 @@ namespace rwsua2017 {
                 listener.waitForTransform(name, "rmartins", now, ros::Duration(time_to_wait));
                 listener.lookupTransform(name, "rmartins", now, trans3);
 
+                listener.waitForTransform(name, "vsilva", now, ros::Duration(time_to_wait));
+                listener.lookupTransform(name, "vsilva", now, trans4);
+
+                listener.waitForTransform(name, "dcorreia", now, ros::Duration(time_to_wait));
+                listener.lookupTransform(name, "dcorreia", now, trans5);
+
+                listener.waitForTransform(name, "jsousa", now, ros::Duration(time_to_wait));
+                listener.lookupTransform(name, "jsousa", now, trans6);
+
             } catch (tf::TransformException ex) {
                 ROS_ERROR("%s", ex.what());
                 ros::Duration(1.0).sleep();
             }
 
 
-            float x1, x2, x3;
-            float y1, y2, y3;
-            float norm1,norm2,norm3;
-            string prey_name;
-            
+            float x1, x2, x3, x4, x5, x6;
+            float y1, y2, y3, y4, y5, y6;
+            float norm1, norm2, norm3, norm4, norm5, norm6;
+
+            string prey_name, hunter_name;
+            float dists[2];
+
+            // ===================== PREYS =============================
             x1 = trans1.getOrigin().x();
             y1 = trans1.getOrigin().y();
 
@@ -207,24 +255,57 @@ namespace rwsua2017 {
             x3 = trans3.getOrigin().x();
             y3 = trans3.getOrigin().y();
 
-            
-            norm1=sqrt(x1*x1 + y1*y1); 
-            norm2=sqrt(x2*x2 + y2*y2); 
-            norm3=sqrt(x3*x3 + y3*y3); 
-            
-            if (norm1<norm2 && norm1<norm3)
-                prey_name="jferreira";
-            else if(norm2<norm1 && norm2<norm3)
-                prey_name="fsilva";
-            else 
-                prey_name="rmartins";
-                    
-            
-                    
-                    
-            return prey_name;
+            norm1 = sqrt(x1 * x1 + y1 * y1);
+            norm2 = sqrt(x2 * x2 + y2 * y2);
+            norm3 = sqrt(x3 * x3 + y3 * y3);
+
+            if (norm1 < norm2 && norm1 < norm3) {
+                prey_name = "jferreira";
+                dists[0] = norm1;
+            } else if (norm2 < norm1 && norm2 < norm3) {
+                prey_name = "fsilva";
+                dists[0] = norm2;
+            } else {
+                prey_name = "rmartins";
+                dists[0] = norm3;
+            }
+
+            // ===================== HUNTERS ========================
+            x4 = trans4.getOrigin().x();
+            y4 = trans4.getOrigin().y();
+
+            x5 = trans5.getOrigin().x();
+            y5 = trans5.getOrigin().y();
+
+            x6 = trans6.getOrigin().x();
+            y6 = trans6.getOrigin().y();
+
+            norm4 = sqrt(x4 * x4 + y4 * y4);
+            norm5 = sqrt(x5 * x5 + y5 * y5);
+            norm6 = sqrt(x6 * x6 + y6 * y6);
 
 
+            if (norm4 < norm5 && norm4 < norm6) {
+                hunter_name = "vsilva";
+                dists[1] = norm4;
+            } else if (norm5 < norm4 && norm5 < norm6) {
+                hunter_name = "dcorreia";
+                dists[1] = norm5;
+            } else {
+                hunter_name = "jsousa";
+                dists[1] = norm6;
+            }
+
+            std::vector<std::string> v;
+            v.push_back(prey_name);
+            v.push_back(hunter_name);
+
+            if (dists[0]>dists[1])
+                v.push_back("run");
+            else
+                v.push_back("hunt");
+
+            return v;
         }
 
         vector<string> teammates;
